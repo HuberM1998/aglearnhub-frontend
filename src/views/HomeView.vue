801cfb9cb56d9c79b81a731584d5ddc2066e6dbf
HuberM1998 <huber.mathaus@gmail.com>
2024-02-12 23:01:36 -0300

    feat: new home page structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <div class="home">
+          <v-card class="card-home" min-width="500" max-width="500" elevation="16">
+            <v-card-title>
+              <v-img class="mx-auto" :width="300" aspect-ratio="16/9" cover
+                src="../assets/logotipo_aglearnhub.jpg"></v-img>
+            </v-card-title>
+            <v-card-item>
+              <v-text-field label="E-mail" :rules="emailRules"></v-text-field>
+              <v-text-field label="Senha" type="password"
+                hint="Entre com a sua senha para acessar a plataforma"></v-text-field>
+            </v-card-item>
+            <p class="login-text-social">
+              Faça login com suas redes sociais:
+            </p>
+            <v-card-item class="footer-login">
+              <v-btn color="red-accent-3" class="buttons-footer" prepend-icon="mdi-google">
+                Google
+              </v-btn>
+              <v-btn class="buttons-footer" prepend-icon="mdi-linkedin">
+                Linkedin
+              </v-btn>
+              <v-btn color="blue-darken-2" class="buttons-footer" prepend-icon="mdi-facebook">
+                Facebook
+              </v-btn>
+            </v-card-item>
+            <v-card-item>
+              <v-btn color="success" class="mt-4" block @click="validate">
+                Entrar
+              </v-btn>
+            </v-card-item>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
+
+<style scoped>
+.home {
+  background: url(../assets/home-bg.jpg);
+  height: 100vh;
+  width: 100%;
+}
+
+.login-text-social{
+  font-size: 0.8em;
+  color: gray;
+}
+.card-home {
+  float: right;
+  margin-top: 10%;
+  margin-right: 10%;
+}
+
+.footer-login .buttons-footer {
+  margin: 10px;
+}
+</style>
